d663714fe7a52057d29007530820a0413a70d5e0
BYVoid <byvoid@byvoid.com>
2013-09-29 17:49:58 +0100

    Add CPS

---
+++ Gist/cps.ml
@@ -0,0 +1,21 @@
+let rec fib num =
+  match num with
+  | 0 -> 1
+  | 1 -> 1
+  | _ -> fib (num - 1) + fib (num - 2)
+
+let rec fib_cps num cont =
+  match num with
+  | 0 -> cont 1
+  | 1 -> cont 1
+  | _ -> fib_cps (num - 1) (fun res1 ->
+    fib_cps (num - 2) (fun res2 ->
+      cont (res1 + res2)
+    )
+  )
+
+let () =
+  Printf.printf "%d\n" (fib 5);
+  fib_cps 5 (fun res ->
+    Printf.printf "%d\n" res
+  )
